b67857a0bda032204c05bb8cbc2be180db3618cd
Ignacio <nachocenturion04@gmail.com>
2024-06-02 16:25:18 -0300

    actualizacion

--- general.js
+++ general.js
@@ -2,26 +2,26 @@ function infoGeneral() {
   const olSubscriptores = document.querySelector("#infoGeneral");
   const subscriptores = JSON.parse(localStorage.getItem("subscriptores")) || [];
 
-  subscriptores.forEach(subscriptor => {
-    olSubscriptores.innerHTML+=`
-        <li>
-            <ul>
-                <li>
-                    ${subscriptor.nombres}
-                </li>
-                <li>
-                    ${subscriptor.gmails}
-                </li>
-                <li>
-                    ${subscriptor.recomendaciones}
-                </li>
-                <li>
-                    ${subscriptores.textos}
-                </li>
-            </ul>
-        </li>
-    `;
-  });
+    subscriptores.forEach(subscriptor => {
+        olSubscriptores.innerHTML+=`
+            <li>
+                <ul>
+                    <li>
+                     ${subscriptor.nombres}
+                    </li>
+                    <li>
+                     ${subscriptor.gmails}
+                    </li>
+                    <li>
+                     ${subscriptor.recomendaciones}
+                    </li>
+                    <li>
+                     ${subscriptor.textos}
+                    </li>
+                </ul>
+            </li>
+        `;
+    });
 }
 
 
